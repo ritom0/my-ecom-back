067d1abc5a807e43fbe7d8c788a82cc682016176
ritom0 <ritom004@gmail.com>
2024-06-16 16:08:24 +0530

    updated

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,6 +1,6 @@
 const Users = require('../models/userModel')
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 
 const userCtrl = {
     register: async (req, res) => {
